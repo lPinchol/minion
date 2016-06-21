b9bb27d47e145230b7805436f204649c30b552a6
kinifi <chrisfigueroa16@gmail.com>
2016-06-21 10:48:54 -0700

    validated paths

--- index.js
+++ index.js
@@ -3,18 +3,78 @@ var fs = require('fs'),
     Jimp = require("jimp");
 
 //set the args
-var srcPath = process.argv[2];
-var dstPath = process.argv[3];
-var newImgWidth = process.argv[4];
-var newImgHeight = process.argv[5];
 
+//the src of the image we need to manipulate
+var type = process.argv[2].toLowerCase();
+var srcPath = process.argv[3];
+//the output folder
+var dstPath = process.argv[4];
+//the new image size
+var newImgWidth = process.argv[5];
+var newImgHeight = process.argv[6];
+//the position in the image to crop from
+var newImgXPos = process.argv[7];
+var newImgYPos = process.argv[8];
+
+var isValidated = false;
+
+//check if the srcpath and dstpath are correct
+validatePaths();
+
+if(type == "crop" || type == "c")
+{
+  if(validatePaths)
+  {
+    CropImage();
+  }
+}
 
 // printArgs();
 // ResizeImage();
 // CropImage();
 
+//checks is a path is a directory or not
+function isDirectory(path)
+{
+  if(fs.lstatSync(path).isDirectory() == true)
+  {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
+//checks is a path is a file or not
+function isFile(path)
+{
+  if(fs.lstatSync(path).isFile() == true)
+  {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
+//makes a directory from a given path
+function makeDirectory(path)
+{
+  try {
+    if(isDirectory(path))
+    {
+      fs.mkdirSync(path);
+    }
+    else {
+      console.log('Not a Directory path');
+    }
+  } catch(e)
+  {
+    if ( e.code != 'EXIST' ) throw e;
+  }
+}
+
+//resize the image
 function ResizeImage()
 {
   // make sure we are using ints
@@ -32,6 +92,7 @@ function ResizeImage()
 
 }
 
+//crop the image
 function CropImage() {
 
   //create a small config
@@ -52,4 +113,35 @@ function printArgs()
   for (var i = 0; i < process.argv.length; i++) {
     console.log('arg' + i + ": " + process.argv[i]);
   }
+}
+
+function validatePaths()
+{
+  var directoryCheck = false;
+  var fileCheck = false;
+
+  if(isDirectory(dstPath) == true)
+  {
+    directoryCheck = true;
+  }
+  else
+  {
+      console.log('dstPath is not valid');
+  }
+
+  if(isFile(srcPath) == true)
+  {
+    fileCheck = true;
+  }
+  else
+  {
+      console.log('srcPath is not valid');
+  }
+
+  if(directoryCheck == true && fileCheck == true)
+  {
+    isValidated = true;
+    console.log('Validated Paths');
+  }
+
 }
